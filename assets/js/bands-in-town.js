927b1de6d0cd8a9b0069b0b89af7b8790ef3380c
John Herman <hermanjohn2@gmail.com>
2020-04-11 10:02:46 -0400

    Can now pull artist info/event info from Bands in Town API.

--- assets/js/bands-in-town.js
+++ assets/js/bands-in-town.js
@@ -1,15 +1,56 @@
-var artist = "Gorillaz";
+function searchByArtist(artist) {
+  var artistURL =
+    "https://rest.bandsintown.com/artists/" + artist + "?app_id=codingbootcamp";
 
-var queryURL =
-  "https://rest.bandsintown.com/artists/" + artist + "?app_id=codingbootcamp";
-
-$.ajax({
-  url: queryURL,
-  method: "GET",
-})
-  .then(function (response) {
-    console.log(response);
+  $.ajax({
+    url: artistURL,
+    method: "GET",
   })
-  .catch(function (err) {
-    console.log(err);
-  });
+    .then(function (response) {
+      // Pulls artist info from API
+      var artistName = response.name;
+      var artistImg = response.thumb_url;
+      // console.log(artistName, artistImg);
+
+      $("#artist-info").append(`
+            
+      <h1>${artistName}</h1>
+      <img src="${artistImg}" />
+      
+      `);
+
+      // Now pull event info from API
+      var eventURL =
+        "https://rest.bandsintown.com/artists/" +
+        artist +
+        "/events?app_id=codingbootcamp";
+
+      $.ajax({
+        url: eventURL,
+        method: "GET",
+      })
+        .then(function (response) {
+          console.log(response.length);
+
+          response.forEach((event) => {
+            console.log(event.venue);
+
+            $("#artist-events").append(`
+            
+            <p>${event.venue.name} | ${event.venue.city}, ${event.venue.region}</p>
+            
+            
+            `);
+          });
+        })
+        .catch();
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
+}
+
+$("#search-btn").on("click", function (event) {
+  event.preventDefault();
+  searchByArtist($("#search-by-artist").val());
+});
