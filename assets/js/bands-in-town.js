10a281e5143c81b271745623f3b7dc62ab009292
John Herman <hermanjohn2@gmail.com>
2020-04-12 19:52:36 -0400

    Pulling more info from Bands In Town API

--- assets/js/bands-in-town.js
+++ assets/js/bands-in-town.js
@@ -1,63 +1,62 @@
 function searchByArtist(artist) {
-  var artistURL =
-    "https://rest.bandsintown.com/artists/" + artist + "?app_id=codingbootcamp";
+  // Now pull event info from API
+  var eventURL =
+    "https://rest.bandsintown.com/artists/" +
+    artist +
+    "/events?app_id=codingbootcamp";
 
   $.ajax({
-    url: artistURL,
+    url: eventURL,
     method: "GET",
   })
     .then(function (response) {
-      // Pulls artist info from API
       console.log(response);
-      var artistName = response.name;
-      var artistImg = response.thumb_url;
 
-      $("#artist-info").append(`
+      for (let i = 0; i < 5; i++) {
+        // Makes Event Date user friendly
+        var eventMonth = response[i].datetime[5] + response[i].datetime[6];
+        var eventDay = response[i].datetime[8] + response[i].datetime[9];
+        var eventYear = response[i].datetime[2] + response[i].datetime[3];
+        var eventDate = eventMonth + "/" + eventDay + "/" + eventYear;
+        // Sets event hour to non-military format to be user friendly
+        var eventHour =
+          response[i].datetime[11] + response[i].datetime[12] - 12;
+        // console.log(eventHour);
+        var eventMinute = response[i].datetime[14] + response[i].datetime[15];
+        var eventTime = eventHour + ":" + eventMinute + "PM";
+
+        // Stores lat and lon of concert location
+        var venueLat = response[i].venue.latitude;
+        var venueLon = response[i].venue.longitude;
+
+        var venueState = response[i].venue.region;
+        var venueCountry = response[i].venue.country;
+
+        var venueLocation = "";
+
+        // If the event is not in the US, it will display the Country...
+        // Else, it will display the State
+        if (venueState === "") {
+          venueLocation += venueCountry;
+        } else {
+          venueLocation += venueState;
+        }
+
+        // console.log("Latitude: " + venueLat);
+        // console.log("Longitude: " + venueLon);
+
+        $("#artist-events").append(`
             
-      <h1>${artistName}</h1>
-      <img src="${artistImg}" />
-      
-      `);
+              <p> ${eventDate} ${eventTime} </p>
 
-      // Now pull event info from API
-      var eventURL =
-        "https://rest.bandsintown.com/artists/" +
-        artist +
-        "/events?app_id=codingbootcamp";
+              <p> ${response[i].venue.name} - ${response[i].venue.city}, ${venueLocation}</p>
 
-      $.ajax({
-        url: eventURL,
-        method: "GET",
-      })
-        .then(function (response) {
-          console.log(response);
-
-          for (let i = 0; i < 5; i++) {
-            // Makes Event Date user friendly
-            var eventMonth = response[i].datetime[5] + response[i].datetime[6];
-            var eventDay = response[i].datetime[8] + response[i].datetime[9];
-            var eventYear = response[i].datetime[2] + response[i].datetime[3];
-            var eventDate = eventMonth + "/" + eventDay + "/" + eventYear;
-            // Sets event hour to non-military format to be user friendly
-            var eventHour =
-              response[i].datetime[11] + response[i].datetime[12] - 12;
-            console.log(eventHour);
-            var eventMinute =
-              response[i].datetime[14] + response[i].datetime[15];
-            var eventTime = eventHour + ":" + eventMinute + "PM";
-
-            $("#artist-events").append(`
-            
-            <p> ${eventDate} ${eventTime} ${response[i].venue.name} ${response[i].venue.city}, ${response[i].venue.region} </p>
+              <a href="${response[i].offers[0].url}">Tickets</a>
                        
             `);
-          }
-        })
-        .catch();
+      }
     })
-    .catch(function (err) {
-      console.log(err);
-    });
+    .catch();
 }
 
 $("#search-btn").on("click", function (event) {
