a4f8d5de56503e58d7948aa5cd3c3ea49f317a4e
John Herman <hermanjohn2@gmail.com>
2020-04-12 19:52:36 -0400

    Pulling more info from Bands In Town API

--- assets/js/bands-in-town.js
+++ assets/js/bands-in-town.js
@@ -1,4 +1,4 @@
-function displayEvents(artist) {
+function searchByArtist(artist) {
   // Now pull event info from API
   var eventURL =
     "https://rest.bandsintown.com/artists/" +
@@ -14,24 +14,32 @@ function displayEvents(artist) {
 
       for (let i = 0; i < 5; i++) {
         // Makes Event Date user friendly
-        var eventDate =
-          response[i].datetime[5] +
-          response[i].datetime[6] +
-          "/" +
-          response[i].datetime[8] +
-          response[i].datetime[9] +
-          "/" +
-          response[i].datetime[2] +
-          response[i].datetime[3];
+        var eventMonth = response[i].datetime[5] + response[i].datetime[6];
+        var eventDay = response[i].datetime[8] + response[i].datetime[9];
+        var eventYear = response[i].datetime[2] + response[i].datetime[3];
+        var eventDate = eventMonth + "/" + eventDay + "/" + eventYear;
+        // Sets event hour to non-military format to be user friendly
+        var eventHour =
+          response[i].datetime[11] + response[i].datetime[12] - 12;
+        // console.log(eventHour);
+        var eventMinute = response[i].datetime[14] + response[i].datetime[15];
+        var eventTime = eventHour + ":" + eventMinute + "PM";
+
+        // Stores lat and lon of concert location
+        var venueLat = response[i].venue.latitude;
+        var venueLon = response[i].venue.longitude;
+
+        var venueState = response[i].venue.region;
+        var venueCountry = response[i].venue.country;
 
         var venueLocation = "";
 
         // If the event is not in the US, it will display the Country...
         // Else, it will display the State
-        if (response[i].venue.region === "") {
-          venueLocation += response[i].venue.country;
+        if (venueState === "") {
+          venueLocation += venueCountry;
         } else {
-          venueLocation += response[i].venue.region;
+          venueLocation += venueState;
         }
 
         // console.log("Latitude: " + venueLat);
@@ -39,65 +47,14 @@ function displayEvents(artist) {
 
         $("#artist-events").append(`
             
-            <ul>
-              <li data-name="${response[i].lineup[0]}"> ${eventDate} ${response[i].venue.city}, ${venueLocation}</li>
-            </ul>
-             
-            `);
-      }
-
-      // Displaying upcoming event
-
-      // Sets Date to user friendly format
-      var eventDate =
-        response[0].datetime[5] +
-        response[0].datetime[6] +
-        "/" +
-        response[0].datetime[8] +
-        response[0].datetime[9] +
-        "/" +
-        response[0].datetime[2] +
-        response[0].datetime[3];
-
-      // Sets event hour to non-military format to be user friendly
-      var eventTime =
-        response[0].datetime[11] +
-        response[0].datetime[12] -
-        12 +
-        ":" +
-        response[0].datetime[14] +
-        response[0].datetime[15] +
-        " PM";
-
-      // Stores lat and lon of concert location ---------------------------------------------------------
-      var venueLat = response[0].venue.latitude;
-      var venueLon = response[0].venue.longitude;
-
-      var venueState = response[0].venue.region;
-      var venueCountry = response[0].venue.country;
-
-      var venueLocation = "";
-
-      // If the event is not in the US, it will display the Country...
-      // Else, it will display the State
-      if (venueState === "") {
-        venueLocation += venueCountry;
-      } else {
-        venueLocation += venueState;
-      }
-
-      $("#main-event").append(`
-            
-            <div>
-            <h1>Upcoming Event</h1>
               <p> ${eventDate} ${eventTime} </p>
 
-              <p> ${response[0].venue.name} - ${response[0].venue.city}, ${venueLocation}</p>
+              <p> ${response[i].venue.name} - ${response[i].venue.city}, ${venueLocation}</p>
 
-              <a href="${response[0].offers[0].url}">Tickets</a>
-            </div>      
-              
+              <a href="${response[i].offers[0].url}">Tickets</a>
+                       
             `);
+      }
     })
     .catch();
 }
