3bde968ec6bf1ece8278820f2eb2330eace35666
Your Name <danygrimaldo@gmail.com>
2020-04-14 15:31:06 -0400

    fixed BIT syntax Issues

--- assets/js/bands-in-town.js
+++ assets/js/bands-in-town.js
@@ -4,14 +4,12 @@ function displayEvents(artist) {
     "https://rest.bandsintown.com/artists/" +
     artist +
     "/events?app_id=codingbootcamp";
-
   $.ajax({
     url: eventURL,
     method: "GET",
   })
     .then(function (response) {
       console.log(response);
-
       for (let i = 0; i < 5; i++) {
         // Makes Event Date user friendly
         var eventDate =
@@ -23,9 +21,7 @@ function displayEvents(artist) {
           "/" +
           response[i].datetime[2] +
           response[i].datetime[3];
-
         var venueLocation = "";
-
         // If the event is not in the US, it will display the Country...
         // Else, it will display the State
         if (response[i].venue.region === "") {
@@ -33,61 +29,58 @@ function displayEvents(artist) {
         } else {
           venueLocation += response[i].venue.region;
         }
-
         // console.log("Latitude: " + venueLat);
         // console.log("Longitude: " + venueLon);
-
         $("#artist-events").append(`
-            
-      <h1>${artistName}</h1>
-      <img src="${artistImg}" />
-      
-      `);
-      console.log()
-
-      // Now pull event info from API
-      var eventURL =
-        "https://rest.bandsintown.com/artists/" +
-        artist +
-        "/events?app_id=codingbootcamp";
-
-      $.ajax({
-        url: eventURL,
-        method: "GET",
-      })
-        .then(function (response) {
-          console.log(response);
-
-          for (let i = 0; i < 5; i++) {
-            // Makes Event Date user friendly
-            var eventMonth = response[i].datetime[5] + response[i].datetime[6];
-            var eventDay = response[i].datetime[8] + response[i].datetime[9];
-            var eventYear = response[i].datetime[2] + response[i].datetime[3];
-            var eventDate = eventMonth + "/" + eventDay + "/" + eventYear;
-            // Sets event hour to non-military format to be user friendly
-            var eventHour =
-              response[i].datetime[11] + response[i].datetime[12] - 12;
-            console.log(eventHour);
-            var eventMinute =
-              response[i].datetime[14] + response[i].datetime[15];
-            var eventTime = eventHour + ":" + eventMinute + "PM";
-
-            $("#artist-events").append(`
-            
+            <ul>
+              <li data-name="${response[i].lineup[0]}"> ${eventDate} ${response[i].venue.city}, ${venueLocation}</li>
+            </ul>
+            `);
+      }
+      // Displaying upcoming event
+      // Sets Date to user friendly format
+      var eventDate =
+        response[0].datetime[5] +
+        response[0].datetime[6] +
+        "/" +
+        response[0].datetime[8] +
+        response[0].datetime[9] +
+        "/" +
+        response[0].datetime[2] +
+        response[0].datetime[3];
+      // Sets event hour to non-military format to be user friendly
+      var eventTime =
+        response[0].datetime[11] +
+        response[0].datetime[12] -
+        12 +
+        ":" +
+        response[0].datetime[14] +
+        response[0].datetime[15] +
+        " PM";
+      // Stores lat and lon of concert location ---------------------------------------------------------
+      var venueLat = response[0].venue.latitude;
+      var venueLon = response[0].venue.longitude;
+      var venueState = response[0].venue.region;
+      var venueCountry = response[0].venue.country;
+      var venueLocation = "";
+      // If the event is not in the US, it will display the Country...
+      // Else, it will display the State
+      if (venueState === "") {
+        venueLocation += venueCountry;
+      } else {
+        venueLocation += venueState;
+      }
+      $("#main-event").append(`
             <div>
             <h1>Upcoming Event</h1>
               <p> ${eventDate} ${eventTime} </p>
-
               <p> ${response[0].venue.name} - ${response[0].venue.city}, ${venueLocation}</p>
-
               <a href="${response[0].offers[0].url}">Tickets</a>
-            </div>      
-              
+            </div>
             `);
     })
     .catch();
 }
-
 $("#search-btn").on("click", function (event) {
   event.preventDefault();
   $("#artist-events, #main-event").empty();
