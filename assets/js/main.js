3dfe74fa906383829ad1073b534913721c9cbd06
Simon <simonanewton@gmail.com>
2020-04-16 13:05:44 -0400

    added artist artwork, added recently searched and most popular section contents

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,6 +8,7 @@ $(document).ready(function () {
 	var topSongs = $("#top-songs");
 	var popularAlbum = $("#popular-album");
 	var similarArtists = $("#similar-artists");
+	var artistImage = $("#artist-image");
 
 	var allEventsDates = $("#all-events-dates");
 
@@ -19,6 +20,8 @@ $(document).ready(function () {
 
 	var recentlySearched = $("#recently-searched");
 	var mostPopular = $("#most-popular");
+
+	var recentArtists;
 
 	var LastFmAPIkey = "b69d917e3739d4f7f4894f4b185cd0db";
 	var BitAPIKey = "codingbootcamp";
@@ -32,19 +35,18 @@ $(document).ready(function () {
 		$.ajax({
 			url: queryURL,
 			method: "GET"
-		})
-			.then(function (response) {
-				popularAlbum.empty();
-				popularAlbum.text("Most Popular Album: ");
+		}).then(function (response) {
+			popularAlbum.empty();
+			popularAlbum.text("Most Popular Album: ");
 
-				var album = $("<a>");
-				album.attr("href", response.topalbums.album[0].url);
-				album.attr("target", "_blank");
-				album.text(response.topalbums.album[0].name);
+			var album = $("<a>");
+			album.attr("href", response.topalbums.album[0].url);
+			album.attr("target", "_blank");
+			album.text(response.topalbums.album[0].name);
 
-				popularAlbum.append(album);
+			popularAlbum.append(album);
 
-			});
+		});
 	}
 
 	function getSimilarArtists(artist) {
@@ -72,6 +74,23 @@ $(document).ready(function () {
 		});
 	}
 
+	function getArtistArtwork(artist) {
+		var queryURL = `https://rest.bandsintown.com/artists/${artist}/events?app_id=${BitAPIKey}`;
+
+		$.ajax({
+			url: queryURL,
+			method: "GET",
+		}).then(function (response) {
+			artistImage.empty();
+
+			var artistArtwork = $("<img>");
+			artistArtwork.attr("src", response[0].artist.thumb_url);
+			artistArtwork.attr("width", "100%");
+
+			artistImage.append(artistArtwork);
+		});
+	}
+
 	function addArtistInfo(artist) {
 		var queryURL = `https://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&artist=${artist}&api_key=${LastFmAPIkey}&format=json`
 
@@ -93,14 +112,14 @@ $(document).ready(function () {
 			topSongs.empty();
 			topSongs.text("More Top Songs: ");
 
-			var maxNum = 4;
-			for (let i = 1; i < 4; i++) {
+			var maxNumDisplay = 4;
+			for (let i = 1; i < maxNumDisplay; i++) {
 				var song = $("<a>");
 				song.attr("href", response.toptracks.track[i].url);
 				song.attr("target", "_blank");
 				song.text(response.toptracks.track[i].name);
 
-				if (i != maxNum - 1) song.append(", ");
+				if (i < maxNumDisplay - 1) song.append(", ");
 
 				topSongs.append(song);
 			}
@@ -108,6 +127,8 @@ $(document).ready(function () {
 			getArtistAlbum(artist);
 
 			getSimilarArtists(artist);
+
+			getArtistArtwork(artist);
 		});
 	}
 
@@ -124,8 +145,7 @@ $(document).ready(function () {
 			style: "border: 0"
 		});
 
-		upcomingMap.removeClass("p-4 text-center");
-		upcomingMap.addClass("p-0");
+		upcomingMap.removeClass("p-4 text-center").addClass("p-0");
 		upcomingMap.append(eventMap);
 	}
 
@@ -141,8 +161,7 @@ $(document).ready(function () {
 			var noEvent = $("<p>").text("No Event").addClass("py-2")
 			var frownyFace = $("<i>").addClass("far fa-frown fa-5x py-2");
 
-			upcomingMap.removeClass("p-0");
-			upcomingMap.addClass("p-4 text-center");
+			upcomingMap.removeClass("p-0").addClass("p-4 text-center");
 			upcomingMap.append(noEvent, frownyFace);
 		}
 
@@ -154,12 +173,8 @@ $(document).ready(function () {
 			else upcomingLocation.text("Event Location: " + event.venue.location);
 
 			if (event.offers[0].status === "available") {
-				console.log("There are tickets available!!!");
-				console.log("Ticket URL:");
-				console.log(event);
-
 				var ticketLink = $("<a>");
-				ticketLink.addClass("btn btn-primary py-1");
+				ticketLink.addClass("btn btn-primary py-1 ml-1");
 				ticketLink.attr("href", event.offers[0].url);
 				ticketLink.attr("target", "_blank");
 				ticketLink.text("Ticket");
@@ -168,9 +183,7 @@ $(document).ready(function () {
 				upcomingTicket.append(ticketLink);
 			}
 
-			else {
-				console.log("There are no tickets available! :(");
-			}
+			else upcomingTicket.text("Ticket Link: N/A");
 
 			addEventMap(event.venue.name);
 		}
@@ -185,8 +198,7 @@ $(document).ready(function () {
 		}).then(function (response) {
 			allEventsDates.empty();
 
-			if (response.length === 0) {
-				console.log("There are no events.");
+			if (!response.length) {
 				allEventsDates.removeClass();
 				allEventsDates.addClass("px-0 pt-4 m-0 text-center");
 				allEventsDates.text("No Events Coming Up...");
@@ -199,18 +211,12 @@ $(document).ready(function () {
 			}
 
 			else {
-				console.log("There are events.");
-
-				if (response.length > 5) response.length = 5;
+				if (response.length > 6) response.length = 6;
 
 				for (let i = 0; i < response.length; i++) {
 					var eventDate = $("<li>");
 					eventDate.addClass("py-2");
-					eventDate.text(moment(response[i].datetime, 'YYYY-MM-DDTHH:mm:ss').format('L') + ` (${response[i].venue.city})`);
-
-					// eventDate.click(function() {
-					// 	displayEvent(response[i]);
-					// });
+					eventDate.text(`${moment(response[i].datetime, 'YYYY-MM-DDTHH:mm:ss').format('L')} (${response[i].venue.city})`);
 
 					allEventsDates.append(eventDate);
 					allEventsDates.removeClass();
@@ -224,57 +230,77 @@ $(document).ready(function () {
 
 	function displayArtist(artist) {
 		addArtistInfo(artist);
-
 		addAllEvents(artist);
 	}
 
+	function addRecentlySearched() {
+		recentArtists = JSON.parse(localStorage.getItem("recentArtists"));
+
+		if (!recentArtists) recentArtists = ["The Weeknd", "Dua Lipa", "Billie Eilish", 
+		"Kanye West", "Drake", "Childish Gambino", "Tame Impala", "Doja Cat", "The Beatles", 
+		"Post Malone", "Ariana Grande", "Lana Del Rey", "Lady Gaga", "Radiohead", "Frank Ocean"]
+
+		recentlySearched.empty();
+
+		for (let i = 0; i < recentArtists.length; i++) {
+			var artistName = $("<li>").text(recentArtists[i]);
+			recentlySearched.append(artistName);
+		}
+	}
+
+	function updateRecentlySearched(artist) {
+		recentArtists.unshift(artist);
+		recentArtists.pop();
+
+		localStorage.setItem("recentArtists", JSON.stringify(recentArtists));
+
+		addRecentlySearched();
+	}
+
 	function enableSearchBar() {
+		searchBar.click(function () {
+			searchBar.val('');
+		});
+
 		searchBtn.click(function (event) {
 			event.preventDefault();
 			displayArtist(searchBar.val());
+			updateRecentlySearched(searchBar.val());
 		});
 	}
 
-	function addRecentlySearched() {
-		// add a list of recently searched artists
-	}
-
 	function addMostPopular() {
-		var queryURL = "";
+		var queryURL = `https://ws.audioscrobbler.com/2.0/?method=chart.gettopartists&api_key=${LastFmAPIkey}&format=json`;
 
 		$.ajax({
 			url: queryURL,
 			method: "GET"
 		}).then(function (response) {
-			// set variable equal to an array of most popular artists
-			var mostPopularArray;
-
-			// set array length to 15
+			var mostPopularArray = response.artists.artist;
 			mostPopularArray.length = 15;
+
+			for (let i = 0; i < mostPopularArray.length; i++) {
+				var artistLi = $("<li>").text(mostPopularArray[i].name);
+				mostPopular.append(artistLi);
+			}
 		});
-
-		for (let i = 0; i < mostPopularArray.length; i++) {
-			var artistLi = $("<li>");
-			artistLi.text(mostPopularArray[i]);
-
-			artistLi.click(function () {
-				displayArtist(artistLi.text());
-			});
-
-			mostPopular.append(artistLi);
-		}
 	}
 
 	//--------------------------------------------------------------
 
 	function main() {
-		console.clear();
+		// localStorage.clear();
+		// console.clear();
 
 		enableSearchBar();
 
-		// addRecentlySearched();
+		addRecentlySearched();
 
-		// addMostPopular();
+		addMostPopular();
+
+		setTimeout(function () {
+			displayArtist("Tame Impala");
+		}, 500);
 	}
 
 	main();
